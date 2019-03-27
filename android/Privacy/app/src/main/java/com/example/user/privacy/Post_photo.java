6044d64ad4f0d2430a67137717f655678cbe57a6
reshmiraju <reshmiraju025@gmail.com>
2019-03-27 19:02:17 +0530

    Create Post_photo.java

---
+++ android/Privacy/app/src/main/java/com/example/user/privacy/Post_photo.java
@@ -0,0 +1,33 @@
+package com.example.user.privacy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Post_photo extends AppCompatActivity {
+ImageView e1;
+EditText e2,e3;
+Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_photo);
+        e1=(ImageView)findViewById(R.id.img);
+        e2=(EditText)findViewById(R.id.content);
+        e3=(EditText)findViewById(R.id.location);
+        b1=(Button)findViewById(R.id.post);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String cnt=e2.getText().toString();
+                String lctn=e3.getText().toString();
+                Toast.makeText(getApplicationContext(),cnt,Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),lctn,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
