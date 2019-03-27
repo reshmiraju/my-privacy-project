25a46c926521b04b28f5bdf080eb6fec7e782dfe
reshmiraju <reshmiraju025@gmail.com>
2019-03-27 19:02:35 +0530

    Create req_send.java

---
+++ android/Privacy/app/src/main/java/com/example/user/privacy/req_send.java
@@ -0,0 +1,13 @@
+package com.example.user.privacy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class req_send extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_req_send);
+    }
+}
