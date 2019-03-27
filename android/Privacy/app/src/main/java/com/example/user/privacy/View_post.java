7cfc70969acd0b6dbdd091b16c5d256e1a51741b
reshmiraju <reshmiraju025@gmail.com>
2019-03-27 19:07:21 +0530

    Create View_post.java

---
+++ android/Privacy/app/src/main/java/com/example/user/privacy/View_post.java
@@ -0,0 +1,16 @@
+package com.example.user.privacy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class View_post extends AppCompatActivity {
+    ListView l1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_post);
+        l1=(ListView)findViewById(R.id.list);
+    }
+}
