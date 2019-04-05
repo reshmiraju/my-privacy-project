d53785c39c73c419eb9319298463b0f9696eb31a
reshmiraju <47964821+reshmiraju@users.noreply.github.com>
2019-04-05 07:14:24 +0530

    Update Register.java

--- android/Privacy/app/src/main/java/com/example/user/privacy/Register.java
+++ android/Privacy/app/src/main/java/com/example/user/privacy/Register.java
@@ -21,9 +21,9 @@ import java.util.ArrayList;
 public class Register extends AppCompatActivity {
 ImageView e1;
 RadioButton r1,r2;
-EditText e3,e4,e5,e6,e7,e8,e9,e10,e11,e12;
+EditText e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e16,e15,e17;
     SharedPreferences sh;
-    String url="";
+    String url="",url1="";
 Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
