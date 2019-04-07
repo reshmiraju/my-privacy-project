da9f75e2bc18481349a28df60b0a73e8d1ccecea
reshmiraju <47964821+reshmiraju@users.noreply.github.com>
2019-04-07 22:35:56 +0530

    Update Chat.java

--- android/Privacy/app/src/main/java/com/example/user/privacy/Chat.java
+++ android/Privacy/app/src/main/java/com/example/user/privacy/Chat.java
@@ -2,7 +2,7 @@ package com.example.user.privacy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-Button b1,b2,b3,b4,b5,b6,b7,b8;
+
 
 public class Chat extends AppCompatActivity {
 
